b79f178f784a0df780a0849c0106bc6f303a694c
Piotrek <phiwanczuk@gmail.com>
2018-01-04 07:56:13 +0100

    grid level added

---
+++ myScript.js
@@ -0,0 +1,9 @@
+var plotno = document.getElementById("plotno");
+var kontekst = plotno.getContext("2d");
+var szerokosc = plotno.width;
+var wysokosc = plotno.height;
+
+var rozmiarBloku = 10;
+var szerokoscWBlokach = szerokosc / rozmiarBloku;
+var wysokoscWWBlokach = wysokosc / rozmiarBloku;
+
