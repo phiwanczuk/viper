f53bdbbcfde3c69739934c0c7fe98f84264e2cb5
Piotrek <phiwanczuk@gmail.com>
2018-01-04 08:01:13 +0100

    level frames

--- myScript.js
+++ myScript.js
@@ -7,3 +7,12 @@ var rozmiarBloku = 10;
 var szerokoscWBlokach = szerokosc / rozmiarBloku;
 var wysokoscWWBlokach = wysokosc / rozmiarBloku;
 
+var wynik = 0;
+
+var rysujObramowanie = function() {
+    kontekst.fillStyle = "Gray";
+    kontekst.fillRect(0,0,szerokosc, rozmiarBloku);
+    kontekst.fillRect(0,wysokosc - rozmiarBloku, szerokosc, rozmiarBloku);
+    kontekst.fillRect(0,0,rozmiarBloku, szerokosc);
+    kontekst.fillRect(szerokosc - rozmiarBloku, 0, rozmiarBloku, wysokosc)
+};
